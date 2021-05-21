f6c2085d0dfd3b4ce72f1af65df208b9daa2c422
Minh Hoàng <51404995+Minh-Hoang98@users.noreply.github.com>
2021-05-21 20:56:41 +0700

    Initial commit

---
+++ Domantic-Helper/src/main/java/com/fpt/edu/DomanticHelper/DomanticHelperApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.DomanticHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomanticHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomanticHelperApplication.class, args);
+	}
+
+}
---
+++ Domantic-Helper/src/test/java/com/fpt/edu/DomanticHelper/DomanticHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.DomanticHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomanticHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
