6aad238ff3ac634bfc3fb3c40e2555b3b853cae0
MinhHoang <hoangvnmde130119@fpt.edu.vn>
2021-07-24 20:55:47 +0700

    update authentication frontend

--- src/components/User/Header/Navbar.js
+++ src/components/User/Header/Navbar.js
@@ -1,17 +1,100 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Navbar.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
-export default function Navbar() {
+import AuthService from "../../../services/auth.service";
+import EventBus from "../../common/EventBus";
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+    
+    EventBus.on("logout", () => {
+      this.logOut();
+    });
+  }
+
+  componentWillUnmount() {
+    EventBus.remove("logout");
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
     return (
-        <div>
+      
+        <nav className="navbar navbar-expand">
+          <Link to={"/"} className="navbar-brand">
+          <input type="checkbox" id="check" />
+    <label htmlFor="check" className="checkbtn">
+      <i className="fa fa-align-justify" />
+    </label>
+     <a href='/home'>
+     <label className="logo" href="#">HelperChoice</label>
+     </a>
+          </Link>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+<ul>
+      <li><NavLink to="/login" className="btLightRun" >
+          <span />
+          <span />
+          <span />
+          <span />
+          {currentUser.username}
+        </NavLink>
+      </li>
+      <li><NavLink to="/logout" className="btLightRun" id="btnSignUp">
+          <span />
+          <span />
+          <span />
+          <span />
+          <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+        </NavLink>
+      </li>
+    </ul>
+            </div>
+          ) : (
+            <div>
   {/* Menu */}
   <nav>
     <input type="checkbox" id="check" />
     <label htmlFor="check" className="checkbtn">
       <i className="fa fa-align-justify" />
     </label>
-    <label className="logo">HelperChoice</label>
+     <a href='/home'>
+     <label className="logo" href="#">HelperChoice</label>
+     </a>
     <ul>
       <li><NavLink to="/login" className="btLightRun" >
           <span />
@@ -32,6 +115,10 @@ export default function Navbar() {
     </ul>
   </nav>
 </div>
-
-    )
+          )}
+        </nav>
+        );
+  }
 }
+
+export default Navbar;
